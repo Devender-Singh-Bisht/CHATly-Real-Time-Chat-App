2f1b65a0a0db4d89eb9711e1429d915d6f2f5441
Devender Singh Bisht <bishtdev293@gmail.com>
2026-01-04 22:11:44 +0530

    fix: Fix the search result to give no results message if no user found on search.

--- frontend/src/pages/FriendShip.jsx
+++ frontend/src/pages/FriendShip.jsx
@@ -38,25 +38,29 @@ function FriendShip() {
                         <SearchBar placeholder="Search Username..." setSearchResults={setSearchResults} />
                         <div className={styles.results}>
                             {searchResults ? (
-                                searchResults?.map((user) => {
-                                    return (
-                                        <div key={user.user_id} className={styles.friend}>
-                                            <div className={styles.avatar}>
-                                                <DefaultProfile fill="black" />
-                                            </div>
-                                            <div className={styles.friendDetail}>
-                                                <h3>{user.first_name + " " + user.last_name}</h3>
-                                                <p>{user.username}</p>
-                                            </div>
-                                            {user.isFriend ? (
-                                                <button className={styles.messageBtn}>Message</button>
+                                searchResults.length === 0 ? (
+                                    <div className={styles.noResults}>No Results!!</div>
+                                ) : (
+                                    searchResults?.map((user) => {
+                                        return (
+                                            <div key={user.user_id} className={styles.friend}>
+                                                <div className={styles.avatar}>
+                                                    <DefaultProfile fill="black" />
+                                                </div>
+                                                <div className={styles.friendDetail}>
+                                                    <h3>{user.first_name + " " + user.last_name}</h3>
+                                                    <p>{user.username}</p>
+                                                </div>
+                                                {user.isFriend ? (
+                                                    <button className={styles.messageBtn}>Message</button>
 
-                                            ) : (
-                                                <button className={styles.addBtn}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80Zm-360-80q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Z" /></svg></button>
-                                            )}
-                                        </div>
-                                    )
-                                })
+                                                ) : (
+                                                    <button className={styles.addBtn}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80Zm-360-80q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Z" /></svg></button>
+                                                )}
+                                            </div>
+                                        )
+                                    })
+                                )
                             ) : (
                                 <div className={styles.noResults}>Search Username...</div>
                             )}
