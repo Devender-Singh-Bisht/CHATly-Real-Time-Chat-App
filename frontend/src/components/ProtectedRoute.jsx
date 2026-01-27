94b5d98e75a32903caa67909672e02631bd69579
Devender Singh Bisht <bishtdev293@gmail.com>
2026-01-27 20:50:57 +0530

    fix: Use the loading state in protected route.

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,12 +1,18 @@
 import { useContext } from "react";
 import { Navigate } from "react-router";
 import { AuthContext } from "../contexts/AuthContext";
+import Spinner from "./Spinner";
 
 export default function ProtectedRoute({ children }) {
 
     const { user } = useContext(AuthContext);
 
-    if (user === null) return null;
-    return user ? children : <Navigate to="/login" replace/>;
+    if (user === null) {
+        return (
+            <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Spinner/>
+            </div>
+        )
+    };
+    return user ? children : <Navigate to="/login" replace />;
 }
- 
