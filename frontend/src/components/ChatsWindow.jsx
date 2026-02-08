9a50691639d70f5a14b161882056a4d8fead9e5a
Devender Singh Bisht <bishtdev293@gmail.com>
2026-02-08 17:59:56 +0530

    feat: Listen to new messages.

--- frontend/src/components/ChatsWindow.jsx
+++ frontend/src/components/ChatsWindow.jsx
@@ -4,11 +4,34 @@ import { ChatContext } from "../contexts/ChatContext";
 import ChatWindowHeader from "./ChatWindowHeader";
 import ChatWindowMessages from "./ChatWindowMessages";
 import ChatWindowInput from "./ChatWindowInput";
+import { useEffect } from "react";
+import { SocketContext } from "../contexts/SocketContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 function ChatsWindow() {
 
+    const { user } = useContext(AuthContext);
     const { chatUser } = useContext(ChatContext);
+    const socket = useContext(SocketContext);
     const [messages, setMessages] = useState({});
+
+    useEffect(() => {
+        socket?.on("new_message", (message) => {
+            const senderId = message["sender_id"];
+            if (!(senderId in messages)) {
+                const newMessage = { id: message["message_id"], text: message["content"], self: message["sender_id"] == user.user_id };
+
+                setMessages(prev => ({
+                    ...prev,
+                    [senderId]: [...(prev[senderId] || []), newMessage],
+                }));
+            }
+        })
+
+        return () => {
+            socket.off("new_message");
+        }
+    }, [socket])
 
     if (chatUser === null) {
         return (
